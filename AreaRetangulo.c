8fadc6076074658212d72b75fa0a07c687ea6426
marc-andrade <61834862+marc-andrade@users.noreply.github.com>
2020-03-05 10:40:31 -0300

    Add files via upload

---
+++ AreaRetangulo.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main() {
+	int base;
+	int altura;
+	printf("Digite a base do retangulo \n");
+	scanf("%d",&base);
+	printf("Digite a altura do retangulo \n");
+	scanf("%d",&altura);
+	printf("A area do seu retangulo eh:%d \n",base*altura);
+	return 0;
+}
